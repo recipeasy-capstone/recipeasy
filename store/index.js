46c2bd5c0f528ffd38cf55b7ae79068a39913ffe
calvin ho <calvinho1214@gmail.com>
2019-01-28 15:40:14 -0500

    added reducers and store

--- store/index.js
+++ store/index.js
@@ -3,8 +3,9 @@ import createLogger from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import user from "./user";
+import recipes from './recipes'
 
-const reducer = combineReducers({ user, product, order });
+const reducer = combineReducers({ user, recipes, order });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
