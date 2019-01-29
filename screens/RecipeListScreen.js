f4d7a975585972e9059c35ec13140c546127345d
Dayoung Lee <dayoung31668@gmail.com>
2019-01-29 17:08:14 -0500

    Added: Recipe List Screen,Camera Screen, edited HomeScreen and NavBar

---
+++ screens/RecipeListScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+import { MonoText } from '../components/StyledText';
+
+export default class RecipeListScreen extends React.Component {
+  static navigationOptions = {
+    title: "Recipe List",
+  };
+
+  render() {
+    const {navigate} = this.props.navigation
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <View style={styles.pantryContainer}>
+
+
+          </View>
+        </ScrollView>
+        <View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  pantryContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  button:{
+    backgroundColor: '#fbfbfb',
+    width:100,
+    marginTop:50,
+    padding: 20,
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 20,
+    //change later
+    fontFamily: 'Helvetica'
+  }
+
+});
