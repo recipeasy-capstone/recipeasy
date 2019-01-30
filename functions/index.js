da4f2e47b3a452f17520509e8b59e872d0300a7e
Melanie Hublard Hershman <melhhersh@gmail.com>
2019-01-30 12:50:13 -0500

    created workaround for secrets and util functions in firestore functions

--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,45 @@
-const functions = require('firebase-functions');
-const { RAPID_API_KEY } = require("../secrets/unirest");
-const unirest = require("unirest");
-const {detectText} = require('../utils')
-const {detectLabels} = require('../utils')
-const path = require('path')
+const functions = require("firebase-functions");
+const uni = require("unirest");
+const { detectText } = require("../utils");
+const { detectLabels } = require("../utils");
+const path = require("path");
 
 exports.getRecipes = functions.https.onRequest((req, res) => {
-    try {
-      let ingredients = encodeURIComponent(req.body.ingredients.join("+"));
-      let recipes =  unirest
-        .get(
-          `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ingredients=${ingredients}`
-        )
-        .header("X-RapidAPI-Key", RAPID_API_KEY)
-        .end((result) => {
-          console.log(result.status, result.headers, result.body);
-        })
-        .then((ref)=>{
-            return res.send(ref);
-        })
-    } catch (err) {
-      console.error(err);
-      res.send(err);
-    }
-  })
+  try {
+    let ingredients = encodeURIComponent(req.body.ingredients.join("+"));
+    let recipes = uni
+      .get(
+        `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ingredients=${ingredients}`
+      )
+      .header("X-RapidAPI-Key", unirest.key)
+      .end(result => {
+        console.log(result.status, result.headers, result.body);
+      })
+      .then(ref => {
+        return res.send(ref);
+      });
+  } catch (err) {
+    console.error(err);
+    res.send(err);
+  }
+});
 
-  exports.detectTexts = functions.https.onRequest((req, res) => {
-    try {
-        const {data} = detectText('./receipt.jpg')
-        .then((ref)=>{
-            return res.send(ref)
-        })
-    }
-    catch (err) {
-        console.error(err)
-    }
-  })
+exports.detectTexts = functions.https.onRequest((req, res) => {
+  try {
+    const { data } = detectText("./receipt.jpg").then(ref => {
+      return res.send(ref);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
 
-  exports.detectLabel = functions.https.onRequest((req,res) => {
-    try {
-        const {data} = detectLabels('./receipt.jpg')
-        .then((ref)=>{
-            return res.send(ref)
-        })
-    }
-    catch (err) {
-        console.error(err)
-    }
-  })
+exports.detectLabel = functions.https.onRequest((req, res) => {
+  try {
+    const { data } = detectLabels("./receipt.jpg").then(ref => {
+      return res.send(ref);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
