186f136d4a9e79e5277499c8b44365ebc5f128cc
Kevin Ko <kevin.hw.ko@gmail.com>
2019-01-30 15:28:19 -0500

    Refactored: Placed fsGetRecipes, fsAutocomplete, fsdetectTexts, fsdetectLabel in secrets/fireFunctions and changed functions/util detectTexts and detectLabels

--- functions/util/detectLabels.js
+++ functions/util/detectLabels.js
@@ -1,17 +1,16 @@
 "use strict";
 
-module.exports = function detectLabels(fileName) {
+module.exports = async function detectLabels(fileName) {
   const vision = require("@google-cloud/vision");
 
   const client = new vision.ImageAnnotatorClient({
     projectId: "receipt-detection-app",
-    keyFilename: "../googleSecret/cloudKey.json"
+    keyFilename: "googleSecret/cloudKey.json"
   });
 
-  const [result] = client.labelDetection(fileName).then(() => {
-    const labels = result.labelAnnotations;
-    return labels.filter(
-      label => label.description === "Receipt" && label.score > 0.5
-    );
-  });
+  const [result] = await client.labelDetection(fileName)
+  const labels = result.labelAnnotations;
+  return labels.filter(
+    label => label.description === "Receipt" && label.score > 0.5
+  );
 };
