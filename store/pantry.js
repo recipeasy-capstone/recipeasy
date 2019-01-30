5b3d565c7e64125f07071c0ca6b4e8ae25912cc7
Melanie Hublard Hershman <melhhersh@gmail.com>
2019-01-30 16:58:23 -0500

    Added: add to pantry thunk

--- store/pantry.js
+++ store/pantry.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { fsdetectTexts, fsdetectLabel } from '../secrets/fireFunctions'
 import autocompleteFunc from '../utils/autocompleteFunc'
 import userInfo from '../utils/firebaseFunc'
+import {firestore} from '../firebaseconfig'
 
 const GOT_INGREDIENTS_LIST = "GOT_INGREDIENTS_LIST"
 const GOT_PANTRY = "GOT_PANTRY"
@@ -10,18 +11,17 @@ const DELETED_FROM_PANTRY = "DELETED_FROM_PANTRY"
 
 const initialState = {
     filteredIngredientList: [],
-    manualIngredientList: [],
     pantry: [],
 }
 
 const gotIngredientsList = filteredIngredientList => ({ type: GOT_INGREDIENTS_LIST, filteredIngredientList })
 const gotPantry = pantry => ({ type: GOT_PANTRY, pantry })
-const addedToPantry = manualIngredientList => ({ type: ADDED_TO_PANTRY, manualIngredientList })
+const addedToPantry = ingredient => ({ type: ADDED_TO_PANTRY, ingredient })
 const deletedFromPantry = ingredient => ({ type: DELETED_FROM_PANTRY, ingredient })
 
 export const fetchIngredientsList = imageURI => async dispatch => {
     try {
-        const isLabel = await axios.post(fsdetectLabel, imageURI)
+        const {isLabel} = await axios.post(fsdetectLabel, imageURI)
         if (isLabel) {
             const {data} = await axios.post(fsdetectTexts, imageURI)
             const words = autocompleteFunc(data)
@@ -40,6 +40,17 @@ export const fetchPantry = userId => async dispatch => {
         dispatch(gotPantry(pantry))
     } 
     catch (error) {
+        console.error(error)
+    }
+}
+
+export const addToPantry = (ingredient, userId) = async dispatch =>{
+    try {
+        await firestore.collection('User').doc(userId).update({
+            pantry: [...pantry, ingredient]
+        })
+        dispatch(addedToPantry(ingredient))
+    } catch (error) {
         console.error(error)
     }
 }
