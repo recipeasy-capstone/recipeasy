c8747f169dc0653cf135cd3ba121af56d8894517
Melanie Hublard Hershman <melhhersh@gmail.com>
2019-01-31 12:04:49 -0500

    recorrected the issue in the store that was a typo

--- store/recipes.js
+++ store/recipes.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import fireData from "../utils/firebaseFunc";
 import axios from "axios";
+import userInfo from "../utils/firebaseFunc";
 import { fsGetRecipes } from "../secrets/fireFunctions";
-=======
-import axios from "axios";
-import userInfo from '../utils/firebaseFunc'
-import { fsGetRecipes } from "../secrets/fireFunctions"
->>>>>>> 0c2e6b69b4af8c81676210b43015575c403c023d
 
 const GOT_ALL_RECIPES = "GOT_ALL_RECIPES";
 const GOT_NEW_RECIPES = "GOT_NEW_RECIPES";
@@ -25,13 +19,8 @@ const addedStarRecipe = starRecipe => ({ type: ADD_STAR_RECIPE, starRecipe });
 //Thunks
 export const fetchAllRecipes = userId => async dispatch => {
   try {
-<<<<<<< HEAD
-    const { data } = await fireData.once("value");
+    const { data } = await userInfo(userId);
     dispatch(gotAllRecipes(data));
-=======
-    const {data} = await userInfo(userId)
-    dispatch(gotAllRecipes(data))
->>>>>>> 0c2e6b69b4af8c81676210b43015575c403c023d
   } catch (error) {
     console.error(error);
   }
@@ -43,11 +32,12 @@ export const fetchNewRecipes = ingredients => async dispatch => {
   } catch (error) {
     console.error(error);
   }
-<<<<<<< HEAD
 };
-export const addingStarRecipe = (recipeId, userId) => async dispatch => {
+export const addingStarRecipe = (recipe, userId) => async dispatch => {
   try {
-    const { data } = await fireData.child("starred").push(recipeId);
+    const { data } = awaituserInfo(userId)
+      .child("starred")
+      .push(recipe);
     dispatch(addedStarRecipe(data));
   } catch (error) {}
 };
@@ -56,22 +46,6 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GOT_ALL_RECIPES:
       return { ...state, allRecipes: action.allRecipes };
-=======
-}
-export const addingStarRecipe = (recipe, userId) => async dispatch => {
-  try {
-    const {data} = awaituserInfo(userId).child('starred').push(recipe)
-    dispatch(addedStarRecipe(data))
-  } catch (error) {
-  }
-}
-
-
-export default function(state = initialState, action) {
-  switch (action.type) {
-    case GOT_All_RECIPES:
-      return {...state, allRecipes: action.allRecipes}
->>>>>>> 0c2e6b69b4af8c81676210b43015575c403c023d
     case GOT_NEW_RECIPES:
       return { ...state, newRecipes: action.newRecipes };
     case ADD_STAR_RECIPE:
