0c8fa35b723eef595232c2286507ee8fbddcb1ab
Melanie Hublard Hershman <melhhersh@gmail.com>
2019-02-01 13:24:39 -0500

    updated recipe functions in firefunction index to point to correct unirest api key

--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,42 @@
-const functions = require('firebase-functions');
-const uni = require('unirest');
-const detectText = require('./util/detectText');
-const detectLabels = require('./util/detectLabels');
-const path = require('path');
+const functions = require("firebase-functions");
+const unirest = require("unirest");
+const detectText = require("./util/detectText");
+const detectLabels = require("./util/detectLabels");
+const path = require("path");
+const unirestKey = require("./googleSecret/unirest");
 
 exports.getRecipes = functions.https.onRequest((req, res) => {
-  try {
-    let ingredients = encodeURIComponent(req.body.join('+'));
-    let recipes = uni
-      .get(
-        `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ingredients=${ingredients}`
-      )
-      .header('X-RapidAPI-Key', unirest.key)
-      .end(result => {
-        console.log(result.status, result.headers, result.body);
-      })
-      .then(ref => {
-        return res.send(ref);
-      });
-  } catch (err) {
-    console.error(err);
-    res.send(err);
-  }
+  let ingredients = encodeURIComponent(req.body.join("+"));
+  unirest
+    .get(
+      `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ingredients=${ingredients}`
+    )
+    .header("X-RapidAPI-Key", unirestKey.RAPID_API_KEY)
+    .end(result => {
+      console.log(result.status, result.headers, result.body);
+    })
+    .then(ref => {
+      return res.send(ref);
+    })
+    .catch(err);
+  console.error(err);
 });
 
 exports.ingredientLookUp = functions.https.onRequest((req, res) => {
-  try {
-    let ingredient = encodeURIComponent(req.body.ingredient);
-    let recipes = uni
-      .get(
-        `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/autocomplete?number=5&query=${ingredient}`
-      )
-      .header('X-RapidAPI-Key', unirest.key)
-      .end(result => {
-        console.log(result.status, result.headers, result.body);
-      })
-      .then(ref => {
-        return res.send(ref);
-      });
-  } catch (err) {
-    console.error(err);
-    res.send(err);
-  }
+  let ingredient = encodeURIComponent(req.body);
+  let recipes = unirest
+    .get(
+      `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/autocomplete?number=5&query=${ingredient}`
+    )
+    .header("X-RapidAPI-Key", unirestKey.RAPID_API_KEY)
+    .end(result => {
+      console.log(result.status, result.headers, result.body);
+    })
+    .then(ref => {
+      return res.send(ref);
+    })
+    .catch(err);
+  console.error(err);
 });
 
 exports.detectTexts = functions.https.onRequest((req, res) => {
