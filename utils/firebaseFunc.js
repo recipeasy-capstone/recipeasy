7855d3e23c4fbe19ef3bb101514ccf39f0d968ec
Kevin Ko <kevin.hw.ko@gmail.com>
2019-02-02 14:22:54 -0500

    Added: Utility functions for addIngredient and deleteIngredient in firebaseFunc

--- utils/firebaseFunc.js
+++ utils/firebaseFunc.js
@@ -10,3 +10,17 @@ export const userInfo = userId => {
     }
   });
 };
+
+export const addIngredient = async (ingredient, userId) => {
+  const currentUser = await firestore.collection('User').doc(userId)
+  currentUser.pantry = [...currentUser.pantry, ingredient]
+
+  await firestore.collection('User').doc(userId).set({currentUser})
+}
+
+export const deleteIngredient = async (ingredient, userId) => {
+  const currentUser = await firestore.collection('User').doc(userId)
+  currentUser.pantry = [...currentUser.pantry].filter(food => food !== ingredient)
+  
+  await firestore.collection('User').doc(userId).set({currentUser})
+}
