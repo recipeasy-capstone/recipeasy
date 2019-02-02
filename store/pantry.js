cb7a4eaa567277b448d545bcedb79e53ce26097c
Kevin Ko <kevin.hw.ko@gmail.com>
2019-02-02 14:15:55 -0500

    Refactored: removed unnecessary imports from pantry.js

--- store/pantry.js
+++ store/pantry.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-import { fsdetectTexts, fsdetectLabel } from '../secrets/fireFunctions';
 import { userInfo } from '../utils/firebaseFunc';
 import { firestore } from '../firebaseconfig';
 
